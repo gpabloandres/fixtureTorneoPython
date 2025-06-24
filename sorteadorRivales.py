0853c2ffdca31cc02aeafdac3cb3f619e7d24263
gpabloandres <gpabloandres@gmail.com>
2025-06-23 21:19:16 -0300

    Actualiza equipo 7mo1ra

--- sorteadorRivales.py
+++ sorteadorRivales.py
@@ -3,7 +3,7 @@ import random
 # Lista de 8 equipos
 equipos = [
     "5º 1º (Alejandro - Camilo)", "5º 2º (Celina - Leandro)", "5º 3º (Juan Guridi - Nahuel Nicolisi)", "6º 1º (Orión - Tomás)",
-    "6º 1º (Morena - Lucia)", "6º 2º (Máximo - Valentino)", "7º 1º (Angel - Alexis)", "7º 2º (Geremias - Tomás)"
+    "6º 1º (Morena - Lucia)", "6º 2º (Máximo - Valentino)", "7º 1º (Angel - Diego)", "7º 2º (Geremias - Tomás)"
 ]
 
 # Verificamos que haya exactamente 8 equipos
